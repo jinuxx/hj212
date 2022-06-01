8c3e48b895f495526e5d349c61b5dbcec7487096
XuJin <marchsong@126.com>
2022-06-01 13:38:41 +0800

    feat: add dependencies and test method

--- src/main/java/com/hj212/format/hbt212/core/T212Mapper.java
+++ src/main/java/com/hj212/format/hbt212/core/T212Mapper.java
@@ -17,6 +17,7 @@ import com.hj212.format.segment.core.feature.SegmentParserFeature;
 import javax.validation.Validator;
 import java.io.*;
 import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
@@ -430,4 +431,13 @@ public class T212Mapper {
         return writeDataAsString(data).toCharArray();
     }
 
+
+    public static void main(String[] args) throws T212FormatException, IOException {
+        String msg = null;
+        msg = "##0453QN=20160801085000001;ST=22;CN=2011;PW=123456;MN=010000A8900016F000169DC0;Flag=7;CP=&&DataTime=20160801084000;a21005-Rtd=1.1,a21005-Flag=N;a21004-Rtd=112,a21004-Flag=N;a21026-Rtd=58,a21026-Flag=N;LA-td=50.1,LA-Flag=N;a34004-Rtd=207,a34004-Flag=N;a34002-Rtd=295,a34002-Flag=N;a01001-Rtd=12.6,a01001-Flag=N;a01002-Rtd=32,a01002-Flag=N;a01006-Rtd=101.02,a01006-Flag=N;a01007-Rtd=2.1,a01007-Flag=N;a01008-Rtd=120,a01008-Flag=N;a34001-Rtd=217,a34001-Flag=N;&&c0c1\\r\\n";
+        // msg = "##0285QN=20190925181031464;ST=22;CN=2061;PW=BF470F88957588DE902D1A52;MN=Z13401000010301;Flag=5;CP=&&DataTime=20190924220000;a34006-Avg=2.69700,a34006-Flag=N;a34007-Avg=7.96600,a34007-Flag=N;a34048-Avg=3.30600,a34048-Flag=N;a34047-Avg=7.35700,a34047-Flag=N;a34049-Avg=10.66300,a34049-Flag=N&&C181\\r\\n";
+        // msg = "##0101QN=20160801085857223;ST=32;CN=1062;PW=100000;MN=010000A8900016F000169DC0;Flag=5;CP=&&RtdInterval=30&&1C80\\r\\n";
+        Data data = new T212Mapper().readData(msg.getBytes(StandardCharsets.UTF_8));
+        System.out.println(new ObjectMapper().writeValueAsString(data));
+    }
 }
